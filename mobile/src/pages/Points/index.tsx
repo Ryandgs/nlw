ec8ee0ad40ddde81c58d0d6f2becc9337c00a728
Ryandgs <ryandouglasdasilva@outlook.com>
2020-06-04 20:08:30 -0300

    Fixes

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import MapView from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+
+const Points = () => {
+  const navigation = useNavigation();
+  
+  function handleNavigationBack() { 
+    navigation.goBack();
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView style={styles.map} />
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <TouchableOpacity style={styles.item} onPress={() => {}}>
+          <SvgUri width={42} height={42} uri="http://url/uploads/lampadas.svg" />
+          <Text style={styles.itemTitle}>Lâmpadas</Text>
+        </TouchableOpacity>  
+      </View>      
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80, 
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
+
+export default Points;
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home';
+import Points from './pages/Points';
+import Detail from './pages/Detail';
+
+const AppStack = createStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator headerMode="none" screenOptions={{ 
+                cardStyle: {
+                    backgroundColor: '#f0f0f5'
+                }
+             }}>
+                <AppStack.Screen name="Home" component={Home} />
+                <AppStack.Screen name="Detail" component={Detail} />
+                <AppStack.Screen name="Points" component={Points} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes;
